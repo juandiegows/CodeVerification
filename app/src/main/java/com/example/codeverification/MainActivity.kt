7e145fe524500bc157d7ac47bad7e3a9dc7db9a8
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-09-27 13:39:16 -0500

    mejorar borrado y documentacion

--- app/src/main/java/com/example/codeverification/MainActivity.kt
+++ app/src/main/java/com/example/codeverification/MainActivity.kt
@@ -25,49 +25,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun lockKeyboard(){
+
+    /**
+     *  bloquea el teclado en todos los EditText
+     * */
+    fun lockKeyboard() {
+        //recorremos todos los editText
         getTextBoxList().forEach {
+            //bloqueamos el teclado
             it.inputType = InputType.TYPE_NULL
         }
     }
-    // obtiene n
+
+    /**
+     *  obtiene una lista con todos los EditText
+     * */
     fun getTextBoxList(): java.util.ArrayList<EditText> = with(binding) {
         return@with arrayListOf(txtNum1, txtNum2, txtNum3, txtNum4)
     }
 
+    /**
+     *  Elimina el text del edit que tiene focus  y si esta vacio entonces elimina el texto de edittext anterior
+     * */
     fun delete() {
+        //buscando el EdiText que tiene el focus
         var text: EditText = getTextBoxList().filter {
             it.hasFocus()
         }.first()
 
+        //obtenemos el index que ocupa el edittext en el array de editText
         var index = getTextBoxList().indexOf(text)
+        //si no esta vacio limpiamos el texto
         if (!text.text.isNullOrEmpty()) {
             text.setText("")
-        }else{
+        } else {
+            //si esta vacio miramos,y si podemos avanzar hacia atras
             if (index > 0) {
+                // si se puede avanzar hacia atras entonces lo hacemos y borramos el texto de esa posicion
                 index--;
-                getTextBoxList().get(index).setText("")
+                getTextBoxList().get(index).apply {
+                    setText("")
+                    requestFocus()
+                }
             }
         }
-        if (index > 0) {
-            index--;
-            getTextBoxList().get(index).requestFocus()
-        }
+
     }
 
+    /**
+     * EScribe el texto de boton que se le dio clic en el edit que tiene el focus
+     * */
     fun toWrite(view: View) {
+        //comprobamos si es un boton
         if (view is Button) {
+            //obtenemos el editText que tiene el focus
             var text: EditText = getTextBoxList().filter {
                 it.hasFocus()
             }.first()
+            //escribimos el texto que tiene el boton en el editText
             text.setText(view.text.toString())
+            //cambiamos el tipo de caja de editText a normal
             text.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            //cambiamos el tipo de editText a password despues de un segundo
             Handler(Looper.myLooper()!!).postDelayed({
                 text.transformationMethod = PasswordTransformationMethod.getInstance()
-            },1000)
-
+            }, 1000)
+            //obtenemos el index de la caja que tiene el focus
             var index = getTextBoxList().indexOf(text)
+            //comprobamos que no se vaya a desbordar el vector
             if (index < getTextBoxList().size - 1) {
+                //cambiamos al siguiente editText y le damos focus
                 index++;
                 getTextBoxList().get(index).requestFocus()
             }
